c44c434a59fce21abd56499842385afe4f1073a4
Showrove Roy <showrove200@gmail.com>
2023-05-14 12:07:09 +0600

    navbar basic done

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#396c03",
+          secondary: "#FF8B00",
+          accent: "#3F3F3F",
+          neutral: "#676767",
+          "base-100": "#fff",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
